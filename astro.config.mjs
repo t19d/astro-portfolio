9b63937d7d7d0809828076a1e7c9336ab073b642
t19d <david_tojo@hotmail.es>
2026-04-02 16:18:47 +0200

    ✨ feat: update MainHeader and MainLayout for improved accessibility and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  devToolbar: {
+    enabled: false
+  }
 });
