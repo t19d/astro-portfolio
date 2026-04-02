58fe0158e527f61a620f383809a3cf5399aaf410
t19d <david_tojo@hotmail.es>
2026-04-02 17:17:35 +0200

    ✨ feat: enhance ExperienceSection with company logos and update animations for improved visual effects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   devToolbar: {
     enabled: false
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, 'src/assets'),
+      }
+    }
   }
 });
